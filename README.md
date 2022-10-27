141979193fa69cc2b2e3eaee397dad3b672fddb7
uruchira <uruchira@gmail.com>
2022-10-27 21:46:17 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,13 @@
+In the project directory, you can run:
+
+```
+yarn install
+yarn start
+```
+
+OR using npm
+
+```
+npm install
+npm start
+```
---
+++ src/App.js
@@ -0,0 +1,72 @@
+import { Box, Flex, HStack, Input, SkeletonText } from "@chakra-ui/react";
+
+import {
+  useJsApiLoader,
+  GoogleMap,
+  Marker,
+  Autocomplete,
+} from "@react-google-maps/api";
+import { useRef, useState } from "react";
+
+const center = { lat: 48.8584, lng: 2.2945 };
+
+function App() {
+  const { isLoaded } = useJsApiLoader({
+    googleMapsApiKey: process.env.REACT_APP_GOOGLE_MAPS_API_KEY,
+    libraries: ["places"],
+  });
+
+  const [map, setMap] = useState(null);
+  const originRef = useRef();
+
+  if (!isLoaded) {
+    return <SkeletonText />;
+  }
+
+  return (
+    <Flex
+      position="relative"
+      flexDirection="column"
+      alignItems="center"
+      h="100vh"
+      w="100vw"
+    >
+      <Box position="absolute" left={0} top={0} h="100%" w="100%">
+        {/* Google Map Box */}
+        <GoogleMap
+          center={center}
+          zoom={15}
+          mapContainerStyle={{ width: "100%", height: "100%" }}
+          options={{
+            zoomControl: false,
+            streetViewControl: false,
+            mapTypeControl: false,
+            fullscreenControl: false,
+          }}
+          onLoad={(map) => setMap(map)}
+        >
+          <Marker position={center} />
+        </GoogleMap>
+      </Box>
+      <Box
+        p={4}
+        borderRadius="lg"
+        m={4}
+        bgColor="white"
+        shadow="base"
+        minW="container.md"
+        zIndex="1"
+      >
+        <HStack spacing={2} justifyContent="space-between">
+          <Box flexGrow={1}>
+            <Autocomplete>
+              <Input type="text" placeholder="Origin" ref={originRef} />
+            </Autocomplete>
+          </Box>
+        </HStack>
+      </Box>
+    </Flex>
+  );
+}
+
+export default App;
